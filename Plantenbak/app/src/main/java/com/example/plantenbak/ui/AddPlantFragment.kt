9e00d70ae65540b1347caa20b4113eae922bf2e7
Jesse <sadsa@dsadas.nl>
2021-01-26 07:45:44 +0100

    fill database

--- Plantenbak/app/src/main/java/com/example/plantenbak/ui/AddPlantFragment.kt
+++ Plantenbak/app/src/main/java/com/example/plantenbak/ui/AddPlantFragment.kt
@@ -4,17 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.plantenbak.R
+import com.example.plantenbak.model.Plant
+import com.example.plantenbak.viewmodel.PlantViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-
+import kotlinx.android.synthetic.main.fragment_add_plant.*
+import java.time.LocalDate
 
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class AddPlantFragment : Fragment() {
+
+    private val viewModel: PlantViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -30,10 +37,30 @@ class AddPlantFragment : Fragment() {
         var fabS = view.findViewById(R.id.fabSave) as FloatingActionButton
 
         fabS.setOnClickListener{
+            addPlant()
             findNavController().popBackStack()
         }
 
+    }
+
+    private fun addPlant(){
+        val title = etTitle.text.toString()
+        val date = LocalDate.of(etYear.text.toString().toInt(), etMonth.text.toString().toInt(), etDay.text.toString().toInt())
+
+        var selectedRB = rgWater.checkedRadioButtonId
+        var rbWater = view?.findViewById(selectedRB) as RadioButton
+        val waterPW = rbWater.text.toString()
+
+        val notification = switchNotificatie.isChecked
+
+        selectedRB = rgZon.checkedRadioButtonId
+        var rbZon = view?.findViewById(selectedRB) as RadioButton
+        val zonlicht = rbZon.text.toString()
+
+        val notitie = etNotities.text.toString()
 
 
+        //Log.d("GAME", "$title $platform $date")
+        viewModel.insertPlant(Plant(title, date, waterPW, notification, zonlicht, notitie))
     }
 }
--- Plantenbak/app/src/main/java/com/example/plantenbak/ui/PlantenbakFragment.kt
+++ Plantenbak/app/src/main/java/com/example/plantenbak/ui/PlantenbakFragment.kt
@@ -1,18 +1,23 @@
 package com.example.plantenbak.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.plantenbak.R
 import com.example.plantenbak.model.Plant
+import com.example.plantenbak.viewmodel.PlantViewModel
 import kotlinx.android.synthetic.main.fragment_plantenbak.*
+import androidx.recyclerview.widget.ItemTouchHelper
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -21,6 +26,8 @@ class PlantenbakFragment : Fragment() {
 
     private val plants = arrayListOf<Plant>()
     private val plantAdapter = PlantAdapter(plants)
+
+    private val viewModel : PlantViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -46,5 +53,45 @@ class PlantenbakFragment : Fragment() {
             DividerItemDecoration(context,
                 DividerItemDecoration.VERTICAL)
         )
+
+        createItemTouchHelper().attachToRecyclerView(rvPlants)
+
+        observeAddGame()
+    }
+
+    private fun observeAddGame(){
+        viewModel.plants.observe(viewLifecycleOwner, Observer { plants ->
+            //Log.d("HELP", "$plants")
+            this.plants.clear()
+            this.plants.addAll(plants)
+            plantAdapter.notifyDataSetChanged()
+
+        })
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+
+        // Callback which is used to create the ItemTouch helper. Only enables left swipe.
+        // Use ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) to also enable right swipe.
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            // Enables or Disables the ability to move items up and down.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+
+                val plantToDelete = plants[position]
+                viewModel.deletePlant(plantToDelete)
+            }
+        }
+
+        return ItemTouchHelper(callback)
     }
 }
