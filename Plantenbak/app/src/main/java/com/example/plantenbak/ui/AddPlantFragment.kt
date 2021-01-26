4ab6d6b5054cc2787ab21b0ba3d85f4a09e016bc
Jesse <sadsa@dsadas.nl>
2021-01-26 06:00:32 +0100

    navigation

--- Plantenbak/app/src/main/java/com/example/plantenbak/ui/AddPlantFragment.kt
+++ Plantenbak/app/src/main/java/com/example/plantenbak/ui/AddPlantFragment.kt
@@ -1,13 +1,14 @@
 package com.example.plantenbak.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.plantenbak.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -24,6 +25,13 @@ class AddPlantFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        var fabS = view.findViewById(R.id.fabSave) as FloatingActionButton
+
+        fabS.setOnClickListener{
+            findNavController().popBackStack()
+        }
+
 
     }
 }
--- Plantenbak/app/src/main/java/com/example/plantenbak/ui/MainActivity.kt
+++ Plantenbak/app/src/main/java/com/example/plantenbak/ui/MainActivity.kt
@@ -6,9 +6,13 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.plantenbak.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +23,25 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            navController.navigate(
+                R.id.action_plantenbakFragment_to_addPlantFragment
+            )
+        }
+
+        fabToggler()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+        navController.addOnDestinationChangedListener { _,       destination, _ ->
+            menu.findItem(R.id.btnBin)?.isVisible = destination.id !in arrayOf(R.id.addPlantFragment)
+        }
         return true
     }
 
@@ -36,5 +54,17 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
 
+    }
+
+    private fun fabToggler() {
+
+        navController.addOnDestinationChangedListener { _,       destination, _ ->
+            if (destination.id in arrayOf(R.id.addPlantFragment)) {
+                findViewById<FloatingActionButton>(R.id.fab).hide()
+
+            } else {
+                findViewById<FloatingActionButton>(R.id.fab).show()
+            }
+        }
     }
 }
